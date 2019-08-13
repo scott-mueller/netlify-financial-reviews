3dd3784cdfe2694b5581d237ea376d43b4cf7fa9
Scott Mueller <scott.mueller@golfchannel.com>
2019-08-13 16:52:03 -0400

    Remove dist from source control

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -1,27 +1,10 @@
 
 
-import React from 'react'
-import universal, { setHasBabelPlugin } from '/Users/a206617801/Documents/personal/react-static/financial-reviews/node_modules/react-universal-component/dist/index.js'
-
-setHasBabelPlugin()
-
-const universalOptions = {
-  loading: () => null,
-  error: props => {
-    console.error(props.error);
-    return <div>An error occurred loading this page's template. More information is available in the console.</div>;
-  },
-  ignoreBabelRename: true
-}
-
-const t_0 = universal(import('../node_modules/react-static/lib/browser/components/Default404'), universalOptions)
-      t_0.template = '../node_modules/react-static/lib/browser/components/Default404'
-      
-
+  
 // Template Map
 export default {
-  '../node_modules/react-static/lib/browser/components/Default404': t_0
+  '../node_modules/react-static/lib/browser/components/Default404': require('../node_modules/react-static/lib/browser/components/Default404').default
 }
-// Not Found Template
-export const notFoundTemplate = "../node_modules/react-static/lib/browser/components/Default404"
+
+export const notFoundTemplate = '../node_modules/react-static/lib/browser/components/Default404'
 
--- dist/templates/styles.eb8f9cc4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{96:function(n,w,o){}}]);
